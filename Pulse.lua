160f1baf65181d9938b0eceeab01b411980d684d
John Wayne <johnluke12012@gmail.com>
2025-01-25 12:24:44 -0600

    Okay

--- Pulse.lua
+++ Pulse.lua
@@ -2,7 +2,11 @@ local Pulse = {}
 local env = getgenv()
 
 local function createUUID()
-    return crypt.random(16)
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+        return string.format('%x', v)
+    end)
 end
 
 local function compressAndEncrypt(value)
