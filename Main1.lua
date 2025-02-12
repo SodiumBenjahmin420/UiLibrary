2cf5c30db9f00780f20fbba73264af4b95ad98f3
SodiumBenjahmin420 <161543165+SodiumBenjahmin420@users.noreply.github.com>
2025-02-11 18:53:21 -0600

    Update and rename Main.lua to Main1.lua

--- Main1.lua
+++ Main1.lua
@@ -85,8 +85,7 @@ function UiLibrary:SetToggleFunction(Callback: Function)
     if Callback then
         self.Signals.ToggleSignal:Connect(Callback)
         print("yes there is a callback")
-    else 
-        print("LE NO CALLBECK DE FRIK")
+        return
     end
 
     print("yes")
