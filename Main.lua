b343fb8ce1bb08c095ade978302e147ebb653c17
SodiumBenjahmin420 <161543165+SodiumBenjahmin420@users.noreply.github.com>
2025-02-11 18:19:31 -0600

    Update and rename Main1.lua to Main.lua

--- Main.lua
+++ Main.lua
@@ -53,7 +53,7 @@ function UiLibrary.new(Title: string)
     local Gui: ScreenGui = loadstring(game:HttpGet("https://raw.githubusercontent.com/SodiumBenjahmin420/UiLibrary/refs/heads/Features/GUI"))()
     Gui.Name = Gui.Name .. CaseId
     local Group = Gossamer:Create(Gui.UiHolder,1,true)
-    local Signal_For_Toggle = Signal.New()
+    local Signal_For_Toggle = Signal.new()
     local self = {
         Title = Title,
         Signals = {
